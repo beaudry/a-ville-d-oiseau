7695985d7e6e7e4b68e0beac59953949ab2a5018
Charles-André Beaudry <ca_beaudry@yahoo.ca>
2023-01-07 01:37:16 -0500

    Ajuster les usings

--- À villes d'oiseau/Program.cs
+++ À villes d'oiseau/Program.cs
@@ -1,11 +1,9 @@
 using A_villes_d_oiseau.Cartes;
 using A_villes_d_oiseau.Villes;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<Cartes>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<VilleService>();
