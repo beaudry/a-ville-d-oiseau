45f522a583f5a124e3a7fdd19f545e8eecc830d2
Charles-André Beaudry <ca_beaudry@yahoo.ca>
2023-01-08 17:17:35 -0500

    Filigrane avec format

---
+++ À villes d'oiseau/Villes/Image.cs
@@ -0,0 +1,9 @@
+﻿namespace A_villes_d_oiseau.Villes
+{
+    public class Image
+    {
+        public string Chemin { get; set; } = default!;
+        public int DecalageHorizontal { get; set; }
+        public int DecalageVertical { get; set; }
+    }
+}
--- À villes d'oiseau/Villes/Ville.cs
+++ À villes d'oiseau/Villes/Ville.cs
@@ -5,7 +5,7 @@ namespace A_villes_d_oiseau.Villes
     public class Ville
     {
         public string Nom { get; set; } = default!;
-        public string Image { get; set; } = default!;
+        public Image Image { get; set; } = default!;
         public GeoCoordinate Coordonnees { get; set; } = default!;
         public string LienWikipedia { get; set; } = default!;
     }
