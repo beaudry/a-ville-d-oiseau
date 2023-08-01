5fed28c0e380c05f7772fdf26d6bcfb0dac8fc24
Charles-André Beaudry <ca_beaudry@yahoo.ca>
2023-08-01 17:05:16 -0400

    Mettre les options dans l'URL

--- À villes d'oiseau/Program.cs
+++ À villes d'oiseau/Program.cs
@@ -1,10 +1,10 @@
-using A_villes_d_oiseau.Cartes;
+using A_villes_d_oiseau;
 using A_villes_d_oiseau.Services;
 using A_villes_d_oiseau.Villes;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<Cartes>("#app");
+builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<VilleService>();
